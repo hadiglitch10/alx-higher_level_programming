fe1ff2ad32f69529886d225016ca2cf53e9cc41c
hadiglitch10 <hadiunknownglitch@gmail.com>
2023-08-15 21:34:25 +0300

    comment

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__"
-    import sys
-        argc = sys.argc
+        import sys
+        argc = len(sys.argv)
         if num == 1:
             print("{} arguments.".format(num - 1))
         elif num == 2:
