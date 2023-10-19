fd96c7cd103b9729e42ba4ea56335a93ddcfc760
hadiglitch10 <hadiunknownglitch@gmail.com>
2023-10-19 20:14:22 +0300

    comment

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- Create mysql user 
+-- create user
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+-- grant privileges
+GRANTS ALL PRIVILEGES ON * . * TO user_0d_1@localhost
