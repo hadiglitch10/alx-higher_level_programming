bfdc5c74c49be8cf0560add8e7852c66ee89a889
hadiglitch10 <hadiunknownglitch@gmail.com>
2023-10-19 20:08:48 +0300

    comment

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- list the privileges of user 1
-SHOW GRANTS FOR user user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_1@localhost;
 -- list the privileges of user 2
-SHOW GRANTS FOR user user_0d_2@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
