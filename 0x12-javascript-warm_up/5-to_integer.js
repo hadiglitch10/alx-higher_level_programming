5b4e402f4a7344842e8b9e7552e7e3fae9c221fd
hadiglitch10 <hadiunknownglitch@gmail.com>
2024-03-11 17:12:19 +0200

    besm allah

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const arg = process.argv[2];
+
+const convertedNumber = parseInt(arg);
+
+if (!isNaN(convertedNumber)) {
+  console.log(`My number: ${convertedNumber}`);
+} else {
+  console.log("Not a number");
+}
