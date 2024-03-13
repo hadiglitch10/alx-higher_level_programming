b33c3bab0d2bd755aecc24130de06b023ec4a639
hadiglitch10 <hadiunknownglitch@gmail.com>
2024-03-13 10:53:41 +0200

    besm allah

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const num = parseInt(process.argv[2]);
+
+function factorial(n) {
+    if (isNaN(n)) {
+      return 1;
+    }
+    if (n <= 1) {
+      return 1;
+    }
+    return n * factorial(n - 1);
+  }
+  
+console.log(factorial(num));
+  
